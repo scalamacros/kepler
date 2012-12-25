bc116c0a4d6093b8c6cc38df20a9ac7f54d6b846
Eugene Burmako <xeno.by@gmail.com>
2012-12-25 18:01:49 +0100

    quasiquotes milestone 1
    No spec yet, just docs at http://docs.scala-lang.org/overviews/macros/quasiquotes.html.
    List of known issues: https://issues.scala-lang.org/secure/IssueNavigator.jspa?mode=hide&requestId=11912.

--- src/compiler/scala/tools/nsc/ast/parser/Parsers.scala
+++ src/compiler/scala/tools/nsc/ast/parser/Parsers.scala
@@ -2082,6 +2082,8 @@ self =>
 
 /* -------- PARAMETERS ------------------------------------------- */
 
+    def allowTypelessParams = false
+
     /** {{{
      *  ParamClauses      ::= {ParamClause} [[nl] `(' implicit Params `)']
      *  ParamClause       ::= [nl] `(' [Params] `)'
@@ -2118,7 +2120,7 @@ self =>
         val name = ident()
         var bynamemod = 0
         val tpt =
-          if (settings.YmethodInfer.value && !owner.isTypeName && in.token != COLON) {
+          if (((settings.YmethodInfer.value && !owner.isTypeName) || allowTypelessParams) && in.token != COLON) {
             TypeTree()
           } else { // XX-METHOD-INFER
             accept(COLON)
