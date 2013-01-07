xeno-by/backupler-2012-12-26

SI-6859 type macros no longer crash sbt
By mimicking alias types, macro types no longer trip a sanity check
in sbt 0.12 and 0.13: https://github.com/harrah/xsbt/blob/ef39aeb9c1ea443c17356695758a31152910c4a7/compile/interface/src/main/scala/xsbt/API.scala#L221.

This workaround makes me sad but it's the only option to make type macros
immediately useful. Principled fix pending.

src/compiler/scala/tools/nsc/interpreter/IMain.scala
@@ -523,8 +523,8 @@ class IMain(initialSettings: Settings, protected val out: JPrintWriter) extends 
 
   // normalize non-public types so we don't see protected aliases like Self
   def normalizeNonPublic(tp: Type) = tp match {
-    case TypeRef(_, sym, _) if sym.isAliasType && !sym.isPublic => tp.normalize
-    case _                                                      => tp
+    case TypeRef(_, sym, _) if sym.isAliasType && !sym.isMacroType && !sym.isPublic => tp.normalize
+    case _                                                                          => tp
   }
 
   /**

src/compiler/scala/tools/nsc/typechecker/Implicits.scala
@@ -203,7 +203,7 @@ trait Implicits {
      case TypeRef(pre, sym, _) =>
        sym.isPackageClass ||
        sym.isModuleClass && isStable(pre) /*||
-       sym.isAliasType && isStable(tp.normalize)*/
+       sym.isAliasType && !sym.isMacroType && isStable(tp.normalize)*/
      case _ => tp.isStable
     }
     def isStablePrefix = isStable(pre)
@@ -484,7 +484,7 @@ trait Implicits {
             loop(restpe, pt)
           else pt match {
             case tr @ TypeRef(pre, sym, args) =>
-              if (sym.isAliasType) loop(tp, pt.normalize)
+              if (sym.isAliasType && !sym.isMacroType) loop(tp, pt.normalize)
               else if (sym.isAbstractType) loop(tp, pt.bounds.lo)
               else {
                 val len = args.length - 1
@@ -1009,7 +1009,7 @@ trait Implicits {
                   getClassParts(tp)
                 args foreach (getParts(_))
               }
-            } else if (sym.isAliasType) {
+            } else if (sym.isAliasType && !sym.isMacroType) {
               getParts(tp.normalize)
             } else if (sym.isAbstractType) {
               getParts(tp.bounds.hi)
@@ -1082,7 +1082,7 @@ trait Implicits {
                     getParts(pre)
                     args foreach getParts
                 }
-            } else if (sym.isAliasType) {
+            } else if (sym.isAliasType && !sym.isMacroType) {
               getParts(tp.normalize)
             } else if (sym.isAbstractType) {
               getParts(tp.bounds.hi)
@@ -1355,7 +1355,7 @@ trait Implicits {
             // hence we don't do `pt.dealias` as we did before, but rather do `pt.betaReduce`
             // unlike `dealias`, `betaReduce` performs at most one step of dealiasing
             // while dealias pops all aliases in a single invocation
-            case sym if sym.isAliasType => materializeImplicit(pt.betaReduce)
+            case sym if sym.isAliasType && !sym.isMacroType => materializeImplicit(pt.betaReduce)
             case _ => SearchFailure
           }
         case _ =>
@@ -1445,7 +1445,7 @@ trait Implicits {
   object ImplicitNotFoundMsg {
     def unapply(sym: Symbol): Option[(Message)] = sym.implicitNotFoundMsg match {
       case Some(m) => Some(new Message(sym, m))
-      case None if sym.isAliasType =>
+      case None if sym.isAliasType && !sym.isMacroType =>
         // perform exactly one step of dealiasing
         // this is necessary because ClassManifests are now aliased to ClassTags
         // but we don't want to intimidate users by showing unrelated error messages

src/compiler/scala/tools/nsc/typechecker/Typers.scala
@@ -226,7 +226,7 @@ trait Typers extends Modes with Adaptations with Tags {
     def dropExistential(tp: Type): Type = tp match {
       case ExistentialType(tparams, tpe) =>
         new SubstWildcardMap(tparams).apply(tp)
-      case TypeRef(_, sym, _) if sym.isAliasType =>
+      case TypeRef(_, sym, _) if sym.isAliasType && !sym.isMacroType =>
         val tp0 = tp.normalize
         val tp1 = dropExistential(tp0)
         if (tp1 eq tp0) tp else tp1
@@ -411,7 +411,7 @@ trait Typers extends Modes with Adaptations with Tags {
             case TypeRef(_, sym, args) =>
               checkNoEscape(sym)
               if (!hiddenSymbols.isEmpty && hiddenSymbols.head == sym &&
-                  sym.isAliasType && sameLength(sym.typeParams, args)) {
+                  sym.isAliasType && !sym.isMacroType && sameLength(sym.typeParams, args)) {
                 hiddenSymbols = hiddenSymbols.tail
                 t.normalize
               } else t
@@ -1035,7 +1035,7 @@ trait Typers extends Modes with Adaptations with Tags {
           adapt(tree setType restpe, mode, pt, original)
         case TypeRef(_, ByNameParamClass, List(arg)) if ((mode & EXPRmode) != 0) => // (2)
           adapt(tree setType arg, mode, pt, original)
-        case tr @ TypeRef(_, sym, _) if sym.isAliasType && tr.normalize.isInstanceOf[ExistentialType] &&
+        case tr @ TypeRef(_, sym, _) if sym.isAliasType && !sym.isMacroType && tr.normalize.isInstanceOf[ExistentialType] &&
           ((mode & (EXPRmode | LHSmode)) == EXPRmode) =>
           adapt(tree setType tr.normalize.skolemizeExistential(context.owner, tree), mode, pt, original)
         case et @ ExistentialType(_, _) if ((mode & (EXPRmode | LHSmode)) == EXPRmode) =>
@@ -3704,7 +3704,7 @@ trait Typers extends Modes with Adaptations with Tags {
       val normalizeLocals = new TypeMap {
         def apply(tp: Type): Type = tp match {
           case TypeRef(pre, sym, args) =>
-            if (sym.isAliasType && containsLocal(tp)) apply(tp.normalize)
+            if (sym.isAliasType && !sym.isMacroType && containsLocal(tp)) apply(tp.normalize)
             else {
               if (pre.isVolatile)
                 InferTypeWithVolatileTypeSelectionError(tree, pre)

scalamacros/kepler

src/compiler/scala/tools/nsc/interpreter/IMain.scala
@@ -523,8 +523,8 @@ class IMain(initialSettings: Settings, protected val out: JPrintWriter) extends 
 
   // normalize non-public types so we don't see protected aliases like Self
   def normalizeNonPublic(tp: Type) = tp match {
-    case TypeRef(_, sym, _) if sym.isAliasType && !sym.isPublic => tp.dealias
-    case _                                                      => tp
+    case TypeRef(_, sym, _) if sym.isAliasType && !sym.isMacroType && !sym.isPublic => tp.dealias
+    case _                                                                          => tp
   }
 
   /**

src/compiler/scala/tools/nsc/typechecker/Implicits.scala
@@ -203,7 +203,7 @@ trait Implicits {
      case TypeRef(pre, sym, _) =>
        sym.isPackageClass ||
        sym.isModuleClass && isStable(pre) /*||
-       sym.isAliasType && isStable(tp.normalize)*/
+       sym.isAliasType && !sym.isMacroType && isStable(tp.normalize)*/
      case _ => tp.isStable
     }
     def isStablePrefix = isStable(pre)
@@ -484,7 +484,7 @@ trait Implicits {
             loop(restpe, pt)
           else pt match {
             case tr @ TypeRef(pre, sym, args) =>
-              if (sym.isAliasType) loop(tp, pt.dealias)
+              if (sym.isAliasType && !sym.isMacroType) loop(tp, pt.dealias)
               else if (sym.isAbstractType) loop(tp, pt.bounds.lo)
               else {
                 val len = args.length - 1
@@ -1006,7 +1006,7 @@ trait Implicits {
                   getClassParts(tp)
                 args foreach (getParts(_))
               }
-            } else if (sym.isAliasType) {
+            } else if (sym.isAliasType && !sym.isMacroType) {
               getParts(tp.dealias)
             } else if (sym.isAbstractType) {
               getParts(tp.bounds.hi)
@@ -1270,7 +1270,7 @@ trait Implicits {
             // hence we don't do `pt.dealias` as we did before, but rather do `pt.betaReduce`
             // unlike `dealias`, `betaReduce` performs at most one step of dealiasing
             // while dealias pops all aliases in a single invocation
-            case sym if sym.isAliasType => materializeImplicit(pt.betaReduce)
+            case sym if sym.isAliasType && !sym.isMacroType => materializeImplicit(pt.betaReduce)
             case _ => SearchFailure
           }
         case _ =>
@@ -1360,7 +1360,7 @@ trait Implicits {
   object ImplicitNotFoundMsg {
     def unapply(sym: Symbol): Option[(Message)] = sym.implicitNotFoundMsg match {
       case Some(m) => Some(new Message(sym, m))
-      case None if sym.isAliasType =>
+      case None if sym.isAliasType && !sym.isMacroType =>
         // perform exactly one step of dealiasing
         // this is necessary because ClassManifests are now aliased to ClassTags
         // but we don't want to intimidate users by showing unrelated error messages

src/compiler/scala/tools/nsc/typechecker/Typers.scala
@@ -226,7 +226,7 @@ trait Typers extends Modes with Adaptations with Tags {
     def dropExistential(tp: Type): Type = tp match {
       case ExistentialType(tparams, tpe) =>
         new SubstWildcardMap(tparams).apply(tp)
-      case TypeRef(_, sym, _) if sym.isAliasType =>
+      case TypeRef(_, sym, _) if sym.isAliasType && !sym.isMacroType =>
         val tp0 = tp.dealias
         val tp1 = dropExistential(tp0)
         if (tp1 eq tp0) tp else tp1
@@ -411,7 +411,7 @@ trait Typers extends Modes with Adaptations with Tags {
             case TypeRef(_, sym, args) =>
               checkNoEscape(sym)
               if (!hiddenSymbols.isEmpty && hiddenSymbols.head == sym &&
-                  sym.isAliasType && sameLength(sym.typeParams, args)) {
+                  sym.isAliasType && !sym.isMacroType && sameLength(sym.typeParams, args)) {
                 hiddenSymbols = hiddenSymbols.tail
                 t.dealias
               } else t
@@ -1035,7 +1035,7 @@ trait Typers extends Modes with Adaptations with Tags {
           adapt(tree setType restpe, mode, pt, original)
         case TypeRef(_, ByNameParamClass, List(arg)) if ((mode & EXPRmode) != 0) => // (2)
           adapt(tree setType arg, mode, pt, original)
-        case tr @ TypeRef(_, sym, _) if sym.isAliasType && tr.dealias.isInstanceOf[ExistentialType] &&
+        case tr @ TypeRef(_, sym, _) if sym.isAliasType && !sym.isMacroType && tr.dealias.isInstanceOf[ExistentialType] &&
           ((mode & (EXPRmode | LHSmode)) == EXPRmode) =>
           adapt(tree setType tr.dealias.skolemizeExistential(context.owner, tree), mode, pt, original)
         case et @ ExistentialType(_, _) if ((mode & (EXPRmode | LHSmode)) == EXPRmode) =>
@@ -3699,7 +3699,7 @@ trait Typers extends Modes with Adaptations with Tags {
       val normalizeLocals = new TypeMap {
         def apply(tp: Type): Type = tp match {
           case TypeRef(pre, sym, args) =>
-            if (sym.isAliasType && containsLocal(tp)) apply(tp.dealias)
+            if (sym.isAliasType && !sym.isMacroType && containsLocal(tp)) apply(tp.dealias)
             else {
               if (pre.isVolatile)
                 InferTypeWithVolatileTypeSelectionError(tree, pre)